ac4fffdfc59cc6ec6359bfc54925a051b07414a1
FrancoisCamus <francois.camus@gmail.com>
2019-06-03 10:24:03 +0200

    added grpc working example

---
+++ GrpcApi/GrpcApiClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Greet;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcApiClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number here must match the port of the gRPC server
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest
+                {
+                    Name = "GreeterClient"
+                });
+
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            await channel.ShutdownAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+The first version of the project is based on the following tutorial:
+https://docs.microsoft.com/en-us/aspnet/core/grpc/basics?view=aspnetcore-3.0
+
+To test the project:
+- in GrpcApi folder run the following to start the GRPC server: dotnet run
+- in GrpcApiClient folder run the following to call server: dotnet run
