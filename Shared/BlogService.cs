5fecb33301e24af104cd829687569ace3ee9231c
FrancoisCamus <francois.camus@gmail.com>
2019-06-25 14:07:22 +0200

    Removed non async methods in BlogService.

--- Shared/BlogService.cs
+++ Shared/BlogService.cs
@@ -16,26 +16,12 @@ namespace Shared
             _context.Populate();
         }
 
-        public List<Author> GetAllAuthors()
-        {
-            return _context.Authors.ToList();
-        }
-
         public async Task<List<Author>> GetAllAuthorsAsync()
         {
             return await _context.Authors
                 .Include(a => a.Posts)
                 .Include(a => a.Socials)
                 .ToListAsync();
-        }
-
-        public Author GetAuthorById(int id)
-        {
-            return _context.Authors
-                .Include(p => p.Posts)
-                .Include(p => p.Socials)
-                .Where(author => author.Id == id)
-                .FirstOrDefault();
         }
 
         public async Task<Author> GetAuthorByIdAsync(int id)
@@ -47,27 +33,12 @@ namespace Shared
                 .FirstOrDefaultAsync();
         }
 
-        public List<Post> GetPostsByAuthor(int id)
-        {
-            return _context.Posts
-                .Include(p => p.Comments)
-                .Where(post => post.Author.Id == id)
-                .ToList();
-        }
-
         public async Task<List<Post>> GetPostsByAuthorAsync(int id)
         {
             return await _context.Posts
                 .Include(p => p.Comments)
                 .Where(post => post.Author.Id == id)
                 .ToListAsync();
-        }
-
-        public List<SocialNetworkProfile> GetSocialNetworkProfilesByAuthor(int id)
-        {
-            return _context.SocialNetworkProfiles
-                .Where(sn => sn.Author.Id == id)
-                .ToList();
         }
 
         public async Task<List<SocialNetworkProfile>> GetSocialNetworkProfilesByAuthorAsync(int id)
@@ -81,14 +52,6 @@ namespace Shared
         {
             await _context.Authors.AddAsync(author);
             await _context.SaveChangesAsync();
-
-            return author;
-        }
-
-        public Author AddAuthor(Author author)
-        {
-            _context.Authors.Add(author);
-            _context.SaveChanges();
 
             return author;
         }
--- Shared/IBlogService.cs
+++ Shared/IBlogService.cs
@@ -6,15 +6,10 @@ namespace Shared
 {
     public interface IBlogService
     {
-        List<Author> GetAllAuthors();
         Task<List<Author>> GetAllAuthorsAsync();
-        Author GetAuthorById(int id);
         Task<Author> GetAuthorByIdAsync(int id);
-        List<Post> GetPostsByAuthor(int id);
         Task<List<Post>> GetPostsByAuthorAsync(int id);
-        List<SocialNetworkProfile> GetSocialNetworkProfilesByAuthor(int id);
         Task<List<SocialNetworkProfile>> GetSocialNetworkProfilesByAuthorAsync(int id);
         Task<Author> AddAuthorAsync(Author author);
-        Author AddAuthor(Author author);
     }
 }
